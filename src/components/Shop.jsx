97e1cb75c21d58c12c2a98e656a744eccc21ab38
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-12-06 13:58:27 +0300

    Complete the shop layout with hardcoded product

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,13 +1,13 @@
 import phone from "../assets/phone.png";
-import {
-  BadgeCheck,
-  PhoneOutgoing,
-  Users,
-  MapPin,
-  Warehouse,
-  Star,
-  ChevronRight,
-} from "lucide-react";
+import tv from "../assets/tv.png";
+import Star from "../assets/icons/Star.svg";
+import Approval from "../assets/icons/Approval.svg";
+import Location from "../assets/icons/Location.svg";
+import Ringer from "../assets/icons/Ringer.svg";
+import ShopIcon from "../assets/icons/Shop.svg";
+import Users from "../assets/icons/Users.svg";
+
+import { ChevronRight , StarIcon} from "lucide-react";
 
 const Shop = () => {
   return (
@@ -39,30 +39,30 @@ const Shop = () => {
             </h1>
             <ul className="grid grid-cols-2 lg:grid-cols-1 text-[10px] lg:text-xl w-full">
               <li className="flex items-center gap-2">
-                <BadgeCheck color="#FF8307" />
+                <img src={Approval} alt="" className="w-[15px] lg:w-[30px]" />
                 <p>Verified</p>
               </li>
               <li className="flex items-center gap-2">
-                <PhoneOutgoing color="#FF8307" />
+                <img src={Ringer} alt="" className="w-[15px] lg:w-[30px]" />
                 <p>O759753332</p>
               </li>
               <li className="flex items-center gap-2">
-                <Users color="#FF8307" />
+                <img src={Users} alt="" className="w-[15px] lg:w-[30px]" />
                 <p>600 Followers</p>
                 <button className="bg-primary-100 text-white w-[64px] lg:w-[134px] lg:h-[36px] h-[17px] rounded-md ml-2">
                   Follow
                 </button>
               </li>
               <li className="flex items-center gap-2">
-                <MapPin color="#FF8307" />
+                <img src={Location} alt="" className="w-[15px] lg:w-[30px]" />
                 <p>Karen, Nairobi</p>
               </li>
               <li className="flex items-center gap-2">
-                <Warehouse color="#FF8307" />
+                <img src={ShopIcon} alt="" className="w-[15px] lg:w-[30px]" />
                 Sawa mall, G14
               </li>
               <li className="flex items-center gap-2">
-                <Star color="#FF8307" />
+                <img src={Star} alt="" className="w-[15px] lg:w-[30px]" />
                 <p>4.5</p>
               </li>
             </ul>
@@ -87,6 +87,41 @@ const Shop = () => {
               <ChevronRight color="#FF8307" />
             </span>
           </header>
+
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-5">
+            <div className="bg-white rounded-[20px]  p-4 max-w-[234px] relative h-[320px] flex flex-col justify-between border border-[#E4E7E9] overflow-hidden">
+              {/* Discount Badge */}
+              <span className="bg-primary-100 text-white absolute px-[10px] py-[5px] text-xs font-semibold rounded-sm">
+                20% OFF
+              </span>
+
+              {/* Centered Image */}
+              <img src={tv} alt="phone" className="mx-auto object-cover mt-3" />
+
+              {/* Rating */}
+              <span className="flex items-center text-[#77878F]">
+                <img src={Star} alt="" className="w-[16px]" />{" "}
+                <img src={Star} alt="" className="w-[16px]" />{" "}
+                <img src={Star} alt="" className="w-[16px]" />{" "}
+                <img src={Star} alt="" className="w-[16px]" />{" "}
+                <StarIcon size={16} color="#FF8307" />
+
+                <span className=" text-xs">(911)</span>
+              </span>
+
+              {/* Clamped Description */}
+              <p className="text-sm font-bold line-clamp-2 ">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Doloribus, repellat?
+              </p>
+
+              {/* Pricing */}
+              <div className=" flex items-center text-[10px] lg:text-xs gap-1">
+                <p className=" text-primary-100 line-through">KSH 15000</p>
+                <p className=" text-blue font-semibold">KSH 12000</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
